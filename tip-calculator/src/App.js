6ff3de4195c82261e3db4c1629a72650ce8080c5
Harsh Vardhan <72147432+harsh1000-hub@users.noreply.github.com>
2024-02-03 13:00:40 +0530

    fix bugs of tip cal

--- tip-calculator/src/App.js
+++ tip-calculator/src/App.js
@@ -2,35 +2,35 @@ import "./App.css";
 import { useState } from "react";
 export default function App() {
   // create global state for InputBox and TipMessage component
-  const [billValue, setBillValue] = useState(Number(0));
+  const [billValue, setBillValue] = useState("");
   // create global state for global state for friend1 tip Suggestions
-  const [friend1Tip, setFriend1Tip] = useState(Number(0));
+  const [friend1Tip, setFriend1Tip] = useState(0);
   // create global state for global state for friend2 tip Suggestions
-  const [friend2Tip, setFriend2Tip] = useState(Number(0));
+  const [friend2Tip, setFriend2Tip] = useState(0);
 
   // handle Reset function
   function handleReset() {
-    setBillValue(0);
+    setBillValue("");
+    setFriend1Tip(0);
+    setFriend2Tip(0);
   }
 
   return (
     <div className="App">
       <span>How much was the bill?</span>
       <InputBox billValue={billValue} handleBillValue={setBillValue} />
-      <FriendSuggestions onTipSelect={setFriend1Tip}>
-        <span>How did you like the services</span>
+      <FriendSuggestions onTipSelect={setFriend1Tip} percentage={friend1Tip}>
+        <span>How did you like the services?</span>
       </FriendSuggestions>
-      <FriendSuggestions onTipSelect={setFriend2Tip}>
-        <span>How did your friend like the services</span>
+      <FriendSuggestions onTipSelect={setFriend2Tip} percentage={friend2Tip}>
+        <span>How did your friend like the services?</span>
       </FriendSuggestions>
       <TipMessage
         billValue={billValue}
         friend1Tip={friend1Tip}
         friend2Tip={friend2Tip}
       />
-      {(billValue || friend1Tip || friend2Tip) > 0 && (
-        <button onClick={handleReset}>Reset</button>
-      )}
+      {billValue > 0 && <button onClick={handleReset}>Reset</button>}
     </div>
   );
 }
@@ -43,7 +43,9 @@ function InputBox({ billValue, handleBillValue }) {
   }
   return (
     <input
+      className="input-box"
       type="text"
+      placeholder="bill value"
       value={billValue}
       onChange={(e) => handleValueChange(e)}
     />
@@ -51,7 +53,7 @@ function InputBox({ billValue, handleBillValue }) {
 }
 
 // Friend Suggestions component
-function FriendSuggestions({ children, onTipSelect }) {
+function FriendSuggestions({ children, onTipSelect, percentage }) {
   // handle tip selection
   function handleTipChange(e) {
     onTipSelect(e.target.value);
@@ -59,11 +61,11 @@ function FriendSuggestions({ children, onTipSelect }) {
   return (
     <div>
       {children}
-      <select onChange={(e) => handleTipChange(e)}>
-        <option value={0}>Dissatisfied (0%)</option>
-        <option value={5}>It was okay (5%)</option>
-        <option value={10}>It was good (10%)</option>
-        <option value={20}>Absolutely amazing (20%)</option>
+      <select value={percentage} onChange={(e) => handleTipChange(e)}>
+        <option value="0">Dissatisfied (0%)</option>
+        <option value="5">It was okay (5%)</option>
+        <option value="10">It was good (10%)</option>
+        <option value="20">Absolutely amazing (20%)</option>
       </select>
     </div>
   );
